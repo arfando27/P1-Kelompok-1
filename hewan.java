4c812126d1137fe9f081e2efdf8193ced5222957
Rahmat Agus Firdaus <40308740+beraskencur@users.noreply.github.com>
2023-10-19 12:22:39 +0800

    Add files via upload

---
+++ hewan.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package percobaan1;
+
+class binatang {
+    String nama;
+    String golongan;
+    String jenis;
+    
+}
+public class hewan{
+    public static void main(String[] args) {
+       hewan agus = new hewan();
+
+        agus.nama = "Harimau";
+        agus.golongan = "Karnivora";
+        agus.jenis = "Vetebrata";
+        System.out.println("Nama Hewan : " +agus.nama);
+        System.out.println("Golongan Hewan : " +agus.golongan);
+        System.out.println("Jenis Hewan : " +agus.jenis);
+        
+    }
+    private String nama;
+    private String golongan;
+    private String jenis;
+    
+}
