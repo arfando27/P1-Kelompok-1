24ac80bf60d400c44949490ca3dc71a89c1f84b1
Alrezadwi <148410097+Alrezadwi@users.noreply.github.com>
2023-10-19 15:21:15 +0800

    Add files via upload

---
+++ p1p.java
@@ -0,0 +1,32 @@
+
+class hwn {
+    String nama;
+    String warna;
+    String makanan,habitat;  
+}
+
+public class p1p {
+
+    public static void main(String[] args) {
+       hwn anjing = new hwn();
+       hwn babi = new hwn();
+
+        anjing.nama= "anjing";
+        anjing.warna = "hitam";
+        anjing.makanan = "tulang";
+        
+        babi.nama= "babi";
+        babi.warna = "merah muda";
+        babi.makanan = "kotoran";
+
+        System.out.println("Nama Hewan : " +anjing.nama);
+        System.out.println("warna Hewan : " +anjing.warna);
+        System.out.println("makanan Hewan : " +anjing.makanan);
+
+
+        System.out.println("------------------------------------" );
+        System.out.println("Nama Hewan : " +babi.nama);
+        System.out.println("warna Hewan : " +babi.warna);
+        System.out.println("makanan Hewan : " +babi.makanan);
+}
+}
