decaad9a9d791ee12ae8918a2d8c7843fd8555b1
Arif Fikron <148291716+Ariffikron@users.noreply.github.com>
2023-10-19 13:43:03 +0800

    Add files via upload

---
+++ p1nomor5_F1B021072.java
@@ -0,0 +1,38 @@
+
+
+class NamaH {
+    String nama;
+ 
+    public NamaH(String n){
+        nama=n;
+    }
+    
+   public void tampilA(){
+       System.out.println("Nama Hewan : " +nama);
+   }
+}
+
+class JenisH {
+    String jenis;
+
+    public JenisH(String j){
+        jenis=j;
+    }
+    
+   public void tampilB(){
+       System.out.println("Jenis Hewan : " +jenis);
+   }
+}
+
+public class nomor5 {
+    public static void main(String[] args) {
+        NamaH ando = new NamaH("Singa");
+        ando.tampilA();
+        JenisH grasa = new JenisH("Karnivora");
+        grasa.tampilB();
+        NamaH arif = new NamaH("Gajah");
+        arif.tampilA();
+        JenisH reja = new JenisH("Herbivora");
+        reja.tampilB();
+    }
+}
