085240ca6760910b370422fa69c1ddfe44f0def0
Alrezadwi <148410097+Alrezadwi@users.noreply.github.com>
2023-10-19 17:14:39 +0800

    Add files via upload

---
+++ P1nomor3_F1B021071.java
@@ -0,0 +1,30 @@
+
+      class hewani {
+    String nama;
+    String jenis;
+    String golongan;
+    String bernafas;
+    int kaki;
+    
+    void tampil(){
+            System.out.println(nama);
+            System.out.println(jenis);
+            System.out.println(golongan);
+            System.out.println(kaki);
+            System.out.println("==========");
+}
+    
+}
+public class P1nomor3_F1B021071{
+    public static void main(String[] args) {
+       hewani ejak = new hewani();
+       ejak.nama = "kucing";
+       ejak.golongan= "Karnivora";
+       ejak.jenis = "Vetebrata";
+       ejak.bernafas = "Paru-paru";
+       ejak.kaki = 4;
+       ejak.tampil();
+        
+    }
+    
+}
