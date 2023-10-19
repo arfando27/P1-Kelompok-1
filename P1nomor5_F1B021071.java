7703634286bcf9198558391f93883a6ee65ce591
Alrezadwi <148410097+Alrezadwi@users.noreply.github.com>
2023-10-19 17:15:25 +0800

    Add files via upload

---
+++ P1nomor5_F1B021071.java
@@ -0,0 +1,44 @@
+
+         /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+class NamaH {
+    String nama;
+ 
+    public NamaH(String n){
+        nama=n;
+    }
+    
+   public void tampilA(){
+       System.out.println("Nama Hewan : " +nama);
+   }
+}
+
+class JenisH {
+    String jenis;
+
+    public JenisH(String j){
+        jenis=j;
+    }
+    
+   public void tampilB(){
+       System.out.println("Jenis Hewan : " +jenis);
+   }
+}
+
+public class P1nomor5_F1B021071 {
+    public static void main(String[] args) {
+        NamaH ando = new NamaH("Singa");
+        ando.tampilA();
+        JenisH grasa = new JenisH("Karnivora");
+        grasa.tampilB();
+        NamaH arif = new NamaH("Gajah");
+        arif.tampilA();
+        JenisH reja = new JenisH("Herbivora");
+        reja.tampilB();
+    }
+}
+
+
